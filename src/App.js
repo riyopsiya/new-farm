886db85b5eab5577a994855e061dcdb489525f99
Hardik Singh <hardiksingh950@gmail.com>
2024-11-03 19:25:22 +0530

    referral added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
@@ -14,33 +13,30 @@ import { login, setUserData } from './store/userSlice';
 import AdminDashboard from './Pages/Admin';
 import service from './appwrite/database';
 import { setPremiumTasks, setSocialTasks } from './store/dataSlice';
-import loadingGif from './images/Animation - 1728853348711.gif'
-
+import loadingGif from './images/Animation - 1728853348711.gif';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
-// Helper function to generate a unique referral code
-const generateReferralCode = () => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let code = '';
-  const codeLength = 8; // Length of the referral code, can be adjusted as needed
+  const generateReferralCode = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let code = '';
+    const codeLength = 8;
 
-  for (let i = 0; i < codeLength; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    code += characters[randomIndex];
-  }
+    for (let i = 0; i < codeLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      code += characters[randomIndex];
+    }
 
-  return code;
-};
+    return code;
+  };
 
   const fetchUserData = async () => {
     try {
-
-      //fetch userinfo fromn telegram
+      // Fetch user info from Telegram
       if (window.Telegram?.WebApp) {
         const user = window.Telegram.WebApp.initDataUnsafe?.user;
 
@@ -48,18 +44,22 @@ const generateReferralCode = () => {
           dispatch(login(user));
           const userId = user.id; // Extract the user ID
 
-
           // Check if user exists in the Appwrite database
           const existingUser = await service.getUser(userId.toString());
-
-
 
           if (existingUser) {
             // User exists, dispatch login with user data
             dispatch(setUserData(existingUser));
           } else {
-            // User doesn't exist
-            showReferralPrompt(userId);
+            // User doesn't exist, check for referral code in the URL
+            const urlParams = new URLSearchParams(window.location.search);
+            const referralCode = urlParams.get('referralCode'); // Use the correct key for your referral code
+
+            if (referralCode) {
+              handleNewUserWithReferral(userId, referralCode);
+            } else {
+              handleNewUser(userId);
+            }
           }
         } else {
           console.log('User data not available');
@@ -72,51 +72,29 @@ const generateReferralCode = () => {
     }
   };
 
-
-  const showReferralPrompt = (userId) => {
-    // Show popup with input field for referral code
-    const hasReferral = window.confirm("Do you have a referral code?");
-
-    if (hasReferral) {
-        const referralCode = prompt("Enter the referral code:");
-
-        if (referralCode) {
-            handleNewUserWithReferral(userId, referralCode);
-        }
-    } else {
-        // User does not have a referral code, create new user normally
-        handleNewUser(userId);
-    }
-};
-
-
-
-const handleNewUser = async (userId) => {
-  const newUser = {
+  const handleNewUser = async (userId) => {
+    const newUser = {
       userID: userId,
       coins: 1000,
       taps: 100,
-      referralCode: generateReferralCode()
+      referralCode: generateReferralCode(),
+    };
+
+    const createdUser = await service.createUser(newUser);
+    dispatch(setUserData(createdUser));
   };
-  
-  const createdUser = await service.createUser(newUser);
-  dispatch(setUserData(createdUser));
-};
 
+  const handleNewUserWithReferral = async (userId, referralCode) => {
+    // Check if the referral code exists and belongs to a valid user
+    const referrer = await service.getUserByReferralCode(referralCode);
 
-
-
-const handleNewUserWithReferral = async (userId, referralCode) => {
-  // Check if the referral code exists and belongs to a valid user
-  const referrer = await service.getUserByReferralCode(referralCode);
-
-  if (referrer) {
+    if (referrer) {
       // Create new user with bonus coins for both users
       const newUser = {
-          userID: userId,
-          coins: 2000, // 1000 + 1000 bonus
-          taps: 100,
-          referralCode: generateReferralCode()
+        userID: userId,
+        coins: 2000, // 1000 + 1000 bonus
+        taps: 100,
+        referralCode: generateReferralCode(),
       };
 
       const createdUser = await service.createUser(newUser);
@@ -127,58 +105,43 @@ const handleNewUserWithReferral = async (userId, referralCode) => {
       await service.updateUser(referrer.userID, { coins: referrer.coins });
 
       console.log("Referral bonus awarded to both users");
-  } else {
+    } else {
       alert("Invalid referral code. Creating new user without referral bonus.");
       handleNewUser(userId);
-  }
-};
+    }
+  };
 
   const fetchTasksData = async () => {
-
     try {
-
-
-      const socialTasks = await service.getAllData('social')
-
+      const socialTasks = await service.getAllData('social');
       if (socialTasks?.documents) {
-
-        dispatch(setSocialTasks(socialTasks?.documents))
+        dispatch(setSocialTasks(socialTasks?.documents));
       }
-      const premiumTasks = await service.getAllData('premium')
-
-
-
-      dispatch(setPremiumTasks(premiumTasks.documents))
-
+      const premiumTasks = await service.getAllData('premium');
+      dispatch(setPremiumTasks(premiumTasks.documents));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
-
-
-
+  };
 
   useEffect(() => {
+    fetchUserData();
+    fetchTasksData();
+  }, []);
 
-    fetchUserData()
-    fetchTasksData()
-
-  }, [loading]);
-
-  if(loading){
-    return <div className="w-screen h-screen flex justify-center items-center">
-    {/* <img src={loadingGif} alt="" className="h-64" /> */}
-    <iframe src="https://lottie.host/embed/330e3274-d251-4bce-a100-07f5cdf0e24c/KqrYQMghNl.json" className='h-48'></iframe>
-  </div>
-  
+  if (loading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <iframe src="https://lottie.host/embed/330e3274-d251-4bce-a100-07f5cdf0e24c/KqrYQMghNl.json" className='h-48'></iframe>
+      </div>
+    );
   }
 
   return (
     <>
       <Router>
-
         <ToastContainer
           className={'text-sm '}
           position="top-center"
@@ -191,12 +154,8 @@ const handleNewUserWithReferral = async (userId, referralCode) => {
           draggable
           pauseOnHover
           theme="dark"
-        // transition: Bounce,
         />
-
         <Header />
-
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tasks" element={<Tasks />} />
@@ -204,201 +163,11 @@ const handleNewUserWithReferral = async (userId, referralCode) => {
           <Route path="/referral" element={<ReferralPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/admin" element={<AdminDashboard />} />
-          {/* 404 Not Found route */}
-          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
-
-        {/* Bottom navigation bar could be static */}
         <BottomNav />
-
-
-
-        {/* <BottomNav /> */}
       </Router>
     </>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-// import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './Pages/Home';
-// import Header from './Components/Header';
-// import BottomNav from './Components/BottomNav';
-// import Tasks from './Pages/Tasks';
-// import Boost from './Pages/Boost';
-// import ReferralPage from './Pages/ReferralPage';
-// import { useEffect, useState } from 'react';
-// import ProfilePage from './Pages/ProfilePage';
-// import { useDispatch } from 'react-redux';
-// import { login, setUserData } from './store/userSlice';
-// import AdminDashboard from './Pages/Admin';
-// import service from './appwrite/database';
-// import { setPremiumTasks, setSocialTasks } from './store/dataSlice';
-// import loadingGif from './images/Animation - 1728853348711.gif'
-
-// import { toast, ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// function App() {
-//   const [loading, setLoading] = useState(true)
-//   const dispatch = useDispatch();
-
-// // Helper function to generate a unique referral code
-// const generateReferralCode = () => {
-//   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//   let code = '';
-//   const codeLength = 8; // Length of the referral code, can be adjusted as needed
-
-//   for (let i = 0; i < codeLength; i++) {
-//     const randomIndex = Math.floor(Math.random() * characters.length);
-//     code += characters[randomIndex];
-//   }
-
-//   return code;
-// };
-
-//   const fetchUserData = async () => {
-//     try {
-
-//       //fetch userinfo fromn telegram
-//       if (window.Telegram?.WebApp) {
-//         const user = window.Telegram.WebApp.initDataUnsafe?.user;
-
-//         if (user) {
-//           dispatch(login(user));
-//           const userId = user.id; // Extract the user ID
-
-
-//           // Check if user exists in the Appwrite database
-//           const existingUser = await service.getUser(userId.toString());
-
-
-
-//           if (existingUser) {
-//             // User exists, dispatch login with user data
-//             dispatch(setUserData(existingUser));
-//           } else {
-//             // User doesn't exist, create a new user in the database
-            
-//             const newUser = {
-//               userID: userId,
-
-//               coins: 1000,
-//               taps: 100,
-//               referralCode: generateReferralCode()
-//             };
-
-//             const createdUser = await service.createUser(newUser);
-//             dispatch(setUserData(createdUser));
-
-//           }
-//         } else {
-//           console.log('User data not available');
-//         }
-//       } else {
-//         console.log('Telegram WebApp not available');
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-
-//   const fetchTasksData = async () => {
-
-//     try {
-
-
-//       const socialTasks = await service.getAllData('social')
-
-//       if (socialTasks?.documents) {
-
-//         dispatch(setSocialTasks(socialTasks?.documents))
-//       }
-//       const premiumTasks = await service.getAllData('premium')
-
-
-
-//       dispatch(setPremiumTasks(premiumTasks.documents))
-
-//     } catch (error) {
-//       console.log(error)
-//     } finally {
-//       setLoading(false)
-//     }
-//   }
-
-
-
-
-//   useEffect(() => {
-
-//     fetchUserData()
-//     fetchTasksData()
-
-//   }, [loading]);
-
-//   if(loading){
-//     return <div className="w-screen h-screen flex justify-center items-center">
-//     {/* <img src={loadingGif} alt="" className="h-64" /> */}
-//     <iframe src="https://lottie.host/embed/330e3274-d251-4bce-a100-07f5cdf0e24c/KqrYQMghNl.json" className='h-48'></iframe>
-//   </div>
-  
-//   }
-
-//   return (
-//     <>
-//       <Router>
-
-//         <ToastContainer
-//           className={'text-sm '}
-//           position="top-center"
-//           autoClose={2000}
-//           hideProgressBar={false}
-//           newestOnTop={false}
-//           closeOnClick
-//           rtl={false}
-//           pauseOnFocusLoss
-//           draggable
-//           pauseOnHover
-//           theme="dark"
-//         // transition: Bounce,
-//         />
-
-//         <Header />
-
-
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/tasks" element={<Tasks />} />
-//           <Route path="/boost" element={<Boost />} />
-//           <Route path="/referral" element={<ReferralPage />} />
-//           <Route path="/profile" element={<ProfilePage />} />
-//           <Route path="/admin" element={<AdminDashboard />} />
-//           {/* 404 Not Found route */}
-//           {/* <Route path="*" element={<NotFound />} /> */}
-//         </Routes>
-
-//         {/* Bottom navigation bar could be static */}
-//         <BottomNav />
-
-
-
-//         {/* <BottomNav /> */}
-//       </Router>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
--- src/Pages/ReferralPage.jsx
+++ src/Pages/ReferralPage.jsx
@@ -2,35 +2,36 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const ReferralPage = () => {
-
     const { userData } = useSelector((state) => state.user);
-    
+
     const handleInviteClick = () => {
         const referralCode = userData?.referralCode;
-        const appInviteLink = "http://t.me/notcoincollablybot/notcoinbot"; // Replace with your actual invite link
-        const message = `🚀 Join me on Bounty Tap and start earning rewards!\n\n🔗 App Link: ${appInviteLink}\n\n💰 Use my referral code to get started: ${referralCode}`;
-    
+        const appInviteLink = "http://t.me/notcoincollablybot/notcoinbot"; // Replace with your actual app link
+        const message = `🚀 Join me on Bounty Tap and start earning rewards!\n\n🔗 App Link: ${appInviteLink}?referralCode=${referralCode}\n\n💰 Use my referral code to get a bonus of 1000 coins: ${referralCode}`;
+
         if (window.Telegram?.WebApp) {
-            window.Telegram.WebApp.openLink(`https://t.me/share/url?url=${encodeURIComponent(message)}`);
+            // Open the Telegram share link directly
+            window.Telegram.WebApp.openLink(`https://t.me/share/url?url=${encodeURIComponent(appInviteLink)}&text=${encodeURIComponent(message)}`);
         } else {
-            console.log("Telegram WebApp not available.");
+            // Fallback for environments not supporting Telegram WebApp
+            const telegramShareUrl = `https://t.me/share/url?url=${encodeURIComponent(appInviteLink)}&text=${encodeURIComponent(message)}`;
+            window.open(telegramShareUrl, '_blank');
         }
     };
 
     return (
-        <div className=" text-white min-h-screen p-6 flex flex-col items-center justify-start gap-8">
-
+        <div className="text-white min-h-screen p-6 flex flex-col items-center justify-start gap-8">
             {/* Title */}
-            <h2 className="text-2xl font-bold mb-4 ">Invite Friends & Earn Rewards</h2>
+            <h2 className="text-2xl font-bold mb-4">Invite Friends & Earn Rewards</h2>
 
             {/* How it works */}
-            <div className="p-4 ">
+            <div className="p-4">
                 <h3 className="text-xl font-semibold text-white mb-4">How it works</h3>
                 <div className="relative pl-6 space-y-8">
                     {/* Step 1 */}
-                    <div className="absolute h-[11rem] w-px bg-orange-500  left-2 top-4"></div>
+                    <div className="absolute h-[11rem] w-px bg-orange-500 left-2 top-4"></div>
                     <div className="flex items-start">
-                        <div className="h-4 w-4 absolute left-[1px] top-1 bg-black  border-[5px] border-[#7d5126] rounded-full"></div>
+                        <div className="h-4 w-4 absolute left-[1px] top-1 bg-black border-[5px] border-[#7d5126] rounded-full"></div>
                         <div className="ml-4">
                             <p className="text-white font-semibold">Share your referral link</p>
                             <p className="text-gray-400">Spread Bounty Task among your friends.</p>
@@ -39,7 +40,7 @@ const ReferralPage = () => {
 
                     {/* Step 2 */}
                     <div className="flex items-start">
-                        <div className="h-4 w-4 absolute left-[1px] bg-black    border-[5px] border-[#7d5126] rounded-full"></div>
+                        <div className="h-4 w-4 absolute left-[1px] bg-black border-[5px] border-[#7d5126] rounded-full"></div>
                         <div className="ml-4">
                             <p className="text-white font-semibold">Your friends join Bounty Task</p>
                             <p className="text-gray-400">And begin farming tokens.</p>
@@ -48,7 +49,7 @@ const ReferralPage = () => {
 
                     {/* Step 3 */}
                     <div className="flex items-start">
-                        <div className="h-4 w-4 absolute left-[1px]  bg-black   border-[5px] border-[#7d5126] rounded-full"></div>
+                        <div className="h-4 w-4 absolute left-[1px] bg-black border-[5px] border-[#7d5126] rounded-full"></div>
                         <div className="ml-4">
                             <p className="text-white font-semibold">Earn 10% from your friend's farming</p>
                             <p className="text-gray-400">Plus an additional 2.5% from their friends.</p>
@@ -58,11 +59,9 @@ const ReferralPage = () => {
             </div>
 
             {/* Invite button */}
-            <button  onClick={handleInviteClick} className="bg-gradient-to-r from-black to-[#7d5126] text-white font-semibold py-2 px-4 rounded-md w-full">
+            <button onClick={handleInviteClick} className="bg-gradient-to-r from-black to-[#7d5126] text-white font-semibold py-2 px-4 rounded-md w-full">
                 Invite Your Friends
             </button>
-
-
         </div>
     );
 };
