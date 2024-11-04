7f9455acf03223201ccab15a7e2f4d4300436754
Hardik Singh <hardiksingh950@gmail.com>
2024-11-04 14:26:38 +0530

    fixed home coin update

--- src/Pages/Boost.jsx
+++ src/Pages/Boost.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import wallet from '../images/wallet.png'
+import comingsoon from '../images/comingsoon.png'
 
 const Boost = () => {
   return (
     <div>
         <div className='flex flex-col min-h-[40rem] justify-center gap-8 items-center '>
-            <img className='h-72' src={wallet } alt="comingsoon" />
+            <img  src={comingsoon } alt="comingsoon" />
 
             <h2 className='text-3xl font-bold'>Coming soon...</h2>
         </div>
