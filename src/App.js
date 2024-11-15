f7e3ecf6c49bbc977d400ca02e280af107254e0d
Hardik Singh <hardiksingh950@gmail.com>
2024-11-15 19:08:19 +0530

    export excel sheet

--- src/App.js
+++ src/App.js
@@ -137,8 +137,8 @@ function App() {
   if (loading) {
     return (
       <div className="w-screen h-screen flex justify-center items-center">
-        {/* loading... */}
-        <iframe src="https://lottie.host/embed/330e3274-d251-4bce-a100-07f5cdf0e24c/KqrYQMghNl.json" className='h-48 '></iframe>
+        loading...
+        {/* <iframe src="https://lottie.host/embed/330e3274-d251-4bce-a100-07f5cdf0e24c/KqrYQMghNl.json" className='h-48 '   sandbox="allow-scripts allow-same-origin allow-forms allow-downloads"></iframe> */}
       </div>
     );
   }
