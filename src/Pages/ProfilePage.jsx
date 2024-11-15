9ffdabbca9d8a7b03ff00009a195d67ee0636820
Hardik Singh <hardiksingh950@gmail.com>
2024-11-15 22:33:45 +0530

    bug fix

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
       <div className="flex items-center justify-between px-4 py-2 border-b border-gray-800">
         
         <h1 className="text-lg font-bold">Profile</h1>
-        {/* <NavLink to={'/admin'} className='p-6 py-2 text-sm bg-gradient-to-r from-black to-[#b1783e] font-semibold  rounded-lg'>Admin</NavLink> */}
+        <NavLink to={'/admin'} className='p-6 py-2 text-sm bg-gradient-to-r from-black to-[#b1783e] font-semibold  rounded-lg'>Admin</NavLink>
        {parseInt(process.env.REACT_APP_ADMIN_ID) ===  userInfo.id ? ( <NavLink to={'/admin'} className='p-6 py-2 text-sm bg-gradient-to-r from-black to-[#b1783e] font-semibold  rounded-lg'>Admin</NavLink>
         ):(null) }
       </div>
